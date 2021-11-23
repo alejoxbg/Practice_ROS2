1104358a539688ee36efb5e58ba12d6bc303ee5d
alejoxbg <alejandro.naranjo_z@uao.edu.co>
2021-11-23 00:51:38 -0500

    added a servie and a node to call that service

--- cpp_pkg/launch/test.launch.py
+++ cpp_pkg/launch/test.launch.py
@@ -6,8 +6,10 @@ cpp_pkg='cpp_pkg'
 def generate_launch_description():
     cpp_pub=Node(package=cpp_pkg,executable='cpp_publisher')
     cpp_sub=Node(package=cpp_pkg,executable='cpp_subscriber')
+    client_cpp=Node(package=cpp_pkg,executable='client_cpp')
 
     return LaunchDescription([
         cpp_sub,
         cpp_pub,
+        client_cpp,
     ])
--- cpp_pkg/src/cpp_pub.cpp
+++ cpp_pkg/src/cpp_pub.cpp
@@ -1,21 +1,26 @@
 #include <chrono>
-#include <functional>
+#include <cinttypes>
 #include <memory>
-#include <string>
 
+#include "example_interfaces/srv/add_two_ints.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
 
+using AddTwoInts = example_interfaces::srv::AddTwoInts;
+using std::placeholders::_1;
+using std::placeholders::_2;
+
 class Cpp_Publisher : public rclcpp::Node
 {
     public:
         Cpp_Publisher()
-        : Node("Cpp_Publisher"), count_(0)
+        : Node("cpp_publisher"), count_(0)
         {
             publisher_=this->create_publisher<std_msgs::msg::String>("cpp_str_msg", 10);
             timer_=this->create_wall_timer(500ms,std::bind(&Cpp_Publisher::timer_callback,this));
+            server_ = this->create_service<AddTwoInts>("add_two_ints", std::bind(&Cpp_Publisher::handle_service,this, _1, _2));
         }
     private:
         void timer_callback()
@@ -25,8 +30,15 @@ class Cpp_Publisher : public rclcpp::Node
             RCLCPP_INFO(this->get_logger(), "Publishing to python: '%s'",message.data.c_str());
             publisher_->publish(message);
         }
+    private:
+         void handle_service(const AddTwoInts::Request::SharedPtr request_message,const AddTwoInts::Response::SharedPtr response_message)
+         {
+            RCLCPP_INFO(this->get_logger(),"request: %" PRId64 " + %" PRId64, request_message->a, request_message->b);
+            response_message->sum = request_message->a + request_message->b;
+         }
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr server_;
     size_t count_;
 };
 
